09ccb199adf7694fc2e8ff052f3360eda212f7d7
AjayShanmugasundram <ajay.shanmugasundram@agilisium.com>
2022-02-08 12:08:00 +0530

    first commit

---
+++ src/CRUD.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function Fetching() {
+  const [post, setPost] = useState([]);
+  useEffect(() => {
+    function fetchget() {
+      axios
+        .get(
+           " http://api.countrylayer.com/v2/all?access_key=ef414b206d85663b55c162855f8216e1"
+        )
+        .then((res) => {
+          // console.log(res)
+          // console.log(res.data)
+          setPost(res.data);
+        })
+        .catch((er) => {
+          console.log("error");
+        });
+    }
+    fetchget();
+  }, []);
+
+  console.log(post);
+
+  return (
+    <div>
+      <h2>FETCHING DATA USING AXIOS</h2>
+      <table className="body1" border="4">
+        <thead>
+          <tr>
+            <td>Country Code</td>
+            <td>Name</td>
+            <td>Capital</td>
+            <td>Region</td>
+          </tr>
+        </thead>
+        <tbody>
+          {post.map((posts) => (
+            <tr key={posts.alpha2Code}>
+              <td>{posts.alpha3Code}</td>
+              <td>{posts.name}</td>
+              <td>{posts.capital}</td>
+              <td>{posts.region}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Fetching;
